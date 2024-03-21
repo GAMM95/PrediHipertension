365a6a0d3bbc33f21a812909d97755219b81bcd2
Jhonatan Mantilla <116129111+GAMM95@users.noreply.github.com>
2024-03-20 22:43:16 -0500

    refactorizacion

--- lib/Models/historial_data.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
-// class HistorialTest {
-//   final Timestamp timestamp;
-//   HistorialTest({
-//     required this.timestamp,
-//   })
-// }
-
